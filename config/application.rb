ac1ba44c0da8affab13bc374aaef95c4e0637b2f
Nick Prestel <nick.prestel@gmail.com>
2015-03-31 18:14:39 -0400

    updated precompile line

--- config/application.rb
+++ config/application.rb
@@ -49,7 +49,7 @@ module Kassi
     config.assets.paths << VENDOR_CSS_PATH
 
     # Define here additional Assset Pipeline Manifests to include to precompilation
-    config.assets.precompile += ['markerclusterer.js', 'communities/custom-style-*', 'ss-*', 'modernizr.min.js', 'mercury.js','jquery-1.7.js']
+    # config.assets.precompile += ['markerclusterer.js', 'communities/custom-style-*', 'ss-*', 'modernizr.min.js', 'mercury.js','jquery-1.7.js']
     config.assets.precompile << /(^[^_\/]|\/[^_])[^\/]*$/
 
     # Read the config from the config.yml
